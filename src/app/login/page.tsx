a87f977000f320bed7c6f29324d09c0e1e778492
Nikhil Singh <nikhilrajput4265@gmail.com>
2025-12-10 00:58:55 +0530

    signup & login complete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,17 +3,42 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [loading, setLoading] = React.useState(false);
 
-  const onLogin = async () => {};
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      // const response = await axios.post("/api/users/login",user)
+      const response = await axios.post("/api/users/login", user, {
+        headers: { "Content-Type": "application/json" },
+      });
+
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      // toast.error(error.message);
+      toast.error(
+        error.response?.data?.message || error.message || "Login failed"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  const buttonDisabled = user.email.length === 0 || user.password.length === 0;
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl mb-4">Login</h1>
+      {/* <h1 className="text-4xl mb-4">Login</h1> */}
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
       <label htmlFor="email">email</label>
       <input
@@ -35,10 +60,24 @@ export default function LoginPage() {
       />
       <button
         onClick={onLogin}
+        disabled={buttonDisabled || loading}
+        className={`p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none 
+    ${
+      buttonDisabled
+        ? "bg-gray-400 cursor-not-allowed"
+        : "bg-blue-500 text-white"
+    }
+  `}
+      >
+        {loading ? "Processing..." : "Login"}
+      </button>
+
+      {/* <button
+        onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
         Login here
-      </button>
+      </button> */}
       <Link href="/signup">Visit Signup page</Link>
     </div>
   );
